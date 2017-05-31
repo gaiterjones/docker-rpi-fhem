82dbac30819102004065800430d086d68fdb7a05
gaiterjones <paj@gaiterjones.com>
2017-05-31 17:16:13 +0200

    31052017

--- build/start-fhem.sh
+++ build/start-fhem.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# check if fhem src present in volume
+# check if fhem src files are present in volume
+# if not present install from tar created by image
+#
+
 installcheck=/opt/fhem/fhem.cfg
 
 set -x
@@ -9,6 +12,8 @@ if [ -e "$installcheck" ]; then
 	echo "fhem found in volume..."
 else 
 	echo "Installing fhem source files into volume..."
+	chown -R fhem.dialout /opt/fhem
+	chmod -R 755 /opt/fhem
     cd /opt/fhem
 	tar -xvf ../fhem.tar
 fi 
